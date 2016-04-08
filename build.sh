666a70ca1b4bff84dbe88bfb2870968cd0388044
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-08 23:13:44 +1200

    Git ignore

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR=`dirname "$0"`
-./PieCrust/bin/chef --root=$DIR/site bake --config=production --output=blog
+./PieCrust/bin/chef --root=$DIR/site purge
+./PieCrust/bin/chef --root=$DIR/site bake --config=production --output=blog --force
 
 ghp-import -n -m "Publishing kinoshita.eti.br" -p -r origin -b gh-pages blog 
