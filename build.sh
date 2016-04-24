9f1115884af677ce400ac89675389f5f8020fdd1
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-24 22:39:15 +1200

    Remove deprecated parameter

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 DIR=`dirname "$0"`
 
 chef --root=$DIR purge
-chef --root=$DIR bake --config=production --output=blog --force
+chef --root=$DIR bake --output=blog --force
 
 ghp-import -n -m "Publishing kinoshita.eti.br" -p -r origin -b gh-pages blog 
