b530bb1864ca3050914005d473b7920c1e4cc341
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-24 22:40:46 +1200

    Include CNAME file

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ DIR=`dirname "$0"`
 
 chef --root=$DIR purge
 chef --root=$DIR bake --output=blog --force
+cp $DIR/CNAME blog/
 
 ghp-import -n -m "Publishing kinoshita.eti.br" -p -r origin -b gh-pages blog 
