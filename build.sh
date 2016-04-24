dde8e8226b19817fe160f6484016599cd832ed99
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-24 18:40:57 +1200

    Migrating the blog from PieCrust 1 (PHP) to PieCrust 2 (Python)

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR=`dirname "$0"`
-./PieCrust/bin/chef --root=$DIR/site purge
-./PieCrust/bin/chef --root=$DIR/site bake --config=production --output=blog --force
-
-ghp-import -n -m "Publishing kinoshita.eti.br" -p -r origin -b gh-pages blog 
