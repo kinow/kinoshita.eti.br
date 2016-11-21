e8490649989a14bd1aebe1e3a3a3034c178ccde9
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-11-21 21:44:58 +1300

    Include CV PDF during upload

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ DIR=`dirname "$0"`
 chef --root=$DIR purge
 chef --root=$DIR bake --output=blog --force
 cp $DIR/CNAME blog/
+cp cv*.pdf blog/
 
 ghp-import -n -m "Publishing kinoshita.eti.br" -p -r origin -b gh-pages blog 
