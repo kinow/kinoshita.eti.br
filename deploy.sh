18db5a480400c2b6456a36e8308ffa1b3ded1ee6
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-11-21 21:48:20 +1300

    Move CV under the about folder to match the current link

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ DIR=`dirname "$0"`
 chef --root=$DIR purge
 chef --root=$DIR bake --output=blog --force
 cp $DIR/CNAME blog/
-cp cv*.pdf blog/cv.pdf
+cp cv*.pdf blog/about/cv.pdf
 
 ghp-import -n -m "Publishing kinoshita.eti.br" -p -r origin -b gh-pages blog 
