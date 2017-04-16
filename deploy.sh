3e164344ae27e4a34e2e1225bc78832061707ec3
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-04-17 00:31:13 +1200

    Fix shellcheck errors

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-DIR=`dirname "$0"`
+DIR=$(dirname "$0")
 
-chef --root=$DIR purge
-chef --root=$DIR bake --output=blog --force
-cp $DIR/CNAME blog/
+chef --root="$DIR" purge
+chef --root="$DIR" bake --output=blog --force
+cp "$DIR/CNAME" "$DIR/blog/"
+cp "$DIR/favicon.ico" "$DIR/blog/"
 
 ghp-import -n -m "Publishing kinoshita.eti.br" -p -r origin -b gh-pages blog 
