fb84da73b2d6116b7d1bbdf7fc9aef10014dace3
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-04-17 23:17:08 +1200

    Adding code to download movies JSON dataset

--- scripts/download_movielens_movies.py
+++ scripts/download_movielens_movies.py
@@ -1,7 +1,77 @@
 #!/usr/bin/env python3
 
+import requests
+import json
+from pprint import pprint as pp
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+USER = os.environ.get("movielens_user")
+PASS = os.environ.get("movielens_pass")
+
+class MovieLens(object):
+
+    def __init__(self):
+        self.base_url = 'https://movielens.org/api'
+        self.timeout = 30.0 #seconds
+        self.headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json;charset=utf-8',
+            'DNT': '1',
+            'Host': 'movielens.org',
+            'Pragma': 'no-cache',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:42.0) Gecko/20100101 Firefox/42.0'
+        }
+
+    def login(self, username, password):
+        self.headers['Referer'] = 'https://movielens.org/login'
+        url = "%s/%s" % (self.base_url, "sessions")
+        payload = {'userName': username, 'password': password}
+        r = requests.post(url, data=json.dumps(payload), headers=self.headers, timeout=self.timeout)
+        return r.cookies
+
+    def explore(self, params, cookies):
+        url = "%s/%s" % (self.base_url, "movies/explore")
+        r = requests.get(url, params=params, cookies=cookies, headers=self.headers, timeout=self.timeout)
+        return r.json()
+
+    def _get_last_page(self, json_data):
+        return int(json_data['data']['pager']['totalPages'])
+
+    def _get_movies(self, json_data):
+        movies = []
+        results = json_data['data']['searchResults']
+        for result in results:
+            movies.append("%s (%s)" % (result['movie']['title'], result['movie']['releaseYear']))
+        return movies
+
+    def list_all_rated_movies(self, cookies):
+        movies = []
+        current_page = 1
+
+        while True:
+            resp = self.explore(params={'hasRated': 'yes', 'sortBy': 'userRatedDate'}, cookies=cookies)
+            movies.extend(self._get_movies(resp))
+            last_page = self._get_last_page(resp)
+            if True or current_page >= last_page:
+                break
+            current_page += 1
+
+        return movies
+
 def main():
-    movies_url = "https://movielens.org/api/movies/explore?hasRated=yes&sortBy=userRatedDate"
+    ml = MovieLens()
+    cookies = ml.login(USER, PASS)
+    movies = ml.list_all_rated_movies(cookies)
+    pp(movies)
 
 if __name__ == '__main__':
     main()
