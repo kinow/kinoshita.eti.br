214d915cad6908d063c11d390cc1a137683fed43
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-07-02 15:19:08 +1200

    Issue #42 robots.txt

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ chef --root="$DIR" purge
 chef --root="$DIR" bake --output=blog --force
 cp "$DIR/CNAME" "$DIR/blog/"
 cp "$DIR/favicon.ico" "$DIR/blog/"
+cp "$DIR/robots.txt" "$DIR/blog/"
 
 ghp-import -n -m "Publishing kinoshita.eti.br" -p -r origin -b gh-pages blog 
