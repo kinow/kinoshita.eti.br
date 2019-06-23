e31fba501d262f4476c6c2f47a18e0ff05885385
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2019-06-24 06:50:36 +1200

    s/pages/_pages

--- scripts/download_movielens_movies.py
+++ scripts/download_movielens_movies.py
@@ -85,7 +85,7 @@ def main():
     cookies = ml.login(USER, PASS)
     movies = ml.list_all_rated_movies(cookies)
     movies = sorted(movies, key=cmp_to_key(locale.strcoll))  # locale-aware sort order
-    movies_html_file = dotenv_path = join(dirname(__file__), '../pages/movies.html')
+    movies_html_file = dotenv_path = join(dirname(__file__), '../_pages/movies.md')
     header = """---
 title: 'Movies'
 author: kinow
