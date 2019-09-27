001c81a56e56646a1e5b708cf737ea0b93c07e66
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-09-27 20:35:28 +1200

    Issue #110 Add maps for free-books

---
+++ maps/README.md
@@ -0,0 +1,9 @@
+# Maps
+
+Maps are created with [QGIS](https://qgis.org/), using the following plugins:
+
+- [QuickMapServices](http://plugins.qgis.org/plugins/quick_map_services/)
+
+Icons/styles from:
+
+- [Maki](https://labs.mapbox.com/maki-icons/) by mapbox, CC0
---
+++ maps/free-books/styles/freebooks_1_style.js
@@ -0,0 +1,39 @@
+var size = 0;
+var placement = 'point';
+
+var style_freebooks_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [11, 11],
+                  scale: 2.727272727272727,
+                  anchor: [15, 15],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/library-11.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
